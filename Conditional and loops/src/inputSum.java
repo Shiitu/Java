4ea5d5329e19db990e9b6e4c390564285c35cec1
Jitender Singh <sharpsailor13@gmail.com>
2021-12-31 13:27:25 +0530

    inputsum

---
+++ Conditional and loops/src/inputSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class inputSum {
+//    Take integer inputs till the user enters 0 and print the sum of all numbers.
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int sum=0;
+        int n;
+                System.out.println("Enter the number ");
+             do{
+                   n =sc.nextInt();
+                 sum =sum + n;
+             }while (n != 0) ;
+                System.out.print(sum+" is the sum ");
+        }
+
+    }
+
