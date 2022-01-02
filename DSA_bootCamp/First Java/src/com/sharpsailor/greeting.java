2fc71d5890aa1bf659db0329526ce91c1b7bdbef
Jitender Singh <73359865+sharpsailor@users.noreply.github.com>
2022-01-02 18:50:31 +0530

    Add files via upload

---
+++ DSA_bootCamp/First Java/src/com/sharpsailor/greeting.java
@@ -0,0 +1,11 @@
+package com.sharpsailor;
+import java.util.Scanner;
+
+public class greeting {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter your name ");
+        String name = sc.nextLine();
+        System.out.println("Hello ! you are welcome to --Hello World-- , "+name);
+    }
+}
