79f67ec5a035ecb61c84d2b9115358a688f79154
Jitender Singh <sharpsailor13@gmail.com>
2022-01-04 22:57:32 +0530

    Java

--- Functions/src/com/sharpsailor/Prime.java
+++ Functions/src/com/sharpsailor/Prime.java
@@ -1,7 +1,24 @@
 package com.sharpsailor;
-
+import java.util.Scanner;
 public class Prime {
     public static void main(String[] args) {
-
+    Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number ");
+        int a = sc.nextInt();
+        inPrime(a);
+    }
+    public static void  inPrime(int a ){
+        int temp = 0;
+        for (int i =2; i<=a-1;i++){
+            if (a%i==0){
+                 temp+=1;
+            }
+        }
+        if(temp==0){
+                System.out.println(a+" is not a prime number ");
+        }
+        else {
+                System.out.println(a+" is not a prime number");
+        }
     }
 }
